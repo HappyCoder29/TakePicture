2a363fd4bf75821c9402d22a358bafcbfda1a1d6
Ashish <ashish@drillmaps.com>
2021-11-04 19:59:56 -0700

    Initial Commit

---
+++ TakePicture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakePicture
+//
+//  Created by Ashish Ashish on 11/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
