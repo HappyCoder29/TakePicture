11fc6bdfff2a85fe034b318319ce77c44f52d623
Ashish <ashish@drillmaps.com>
2021-11-04 20:45:47 -0700

    Checking in code for taking picture

---
+++ TakePicture/Utilities/LocalizationUtil.swift
@@ -0,0 +1,10 @@
+//
+//  LocalizationUtil.swift
+//  TakePicture
+//
+//  Created by Ashish Ashish on 11/4/21.
+//
+
+import Foundation
+
+var strTakePicture = NSLocalizedString("take_picture", comment: "")
--- TakePicture/ViewController.swift
+++ TakePicture/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var btnTakePic: UIButton!
+    @IBOutlet weak var imgView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        btnTakePic.titleLabel?.text = strTakePicture    
     }
 
+    @IBAction func takePic(_ sender: Any) {
+        let alertController = UIAlertController(title: "Take a Picture", message: "Select Camera or Photo Library", preferredStyle: .alert)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { action in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = UIImagePickerController.SourceType.camera
+                imagePicker.allowsEditing = false
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+    
+        }
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { action in
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+                imagePicker.allowsEditing = false
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+            
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { action in
+            print("User selected Cancel")
+    
+        }
+        
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cancel)
+        
+        self.present(alertController, animated: true, completion: nil)
 
+
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imgView.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
